f2f62be96bfcfcec4c7b4c322136b40e276c635c
ms8565 <ms8565@NM2550-23>
2017-03-21 17:03:16 -0400

    More models

---
+++ src/StudentEnrollment/Models/Prerequisite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Prerequisite
+    {
+        public int Course_ID { get; set; }
+        public int Day_of_week { get; set; }
+    }
+}
---
+++ src/StudentEnrollment/Models/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Schedule
+    {
+        public int Section_ID { get; set; }
+        public String Day_of_week { get; set; }
+        public TimeSpan Start_time { get; set; }
+        public TimeSpan End_time { get; set; }
+    }
+}
