e1d2dc6ff2bc712ef9b181f3fc928b49abadc8da
Hunter Caskey <huntercaskey@gmail.com>
2017-03-21 15:40:22 -0400

    Test Commit

--- src/StudentEnrollment/Startup.cs
+++ src/StudentEnrollment/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+// Hunter test
 namespace StudentEnrollment
 {
     public class Startup
