3737fb385e1b07af8639124b57f6369110dc14a7
Hunter Caskey <hunterrcaskey@gmail.com>
2017-03-21 16:21:38 -0400

    Added Models folder via upload.
    Models folder contains Admin, Instructor, Student, Course, and User classes.

---
+++ src/StudentEnrollment/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Couse
+    {
+        public int ID { get; set; }
+        public String CourseCode { get; set; }
+        public String Name { get; set; }
+        public int Credits { get; set; }
+        public int MinGPA { get; set; }
+    }
+}
---
+++ src/StudentEnrollment/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudentEnrollment.Models
+{
+    public abstract class User
+    {
+        public int ID { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
