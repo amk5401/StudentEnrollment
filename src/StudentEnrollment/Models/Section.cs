7d671d2e7443e9f13cd77e1f22d31f5ec3b369fc
Hunter Caskey <huntercaskey@gmail.com>
2017-03-21 16:43:13 -0400

    Adding Section to the Models folder.

---
+++ src/StudentEnrollment/Models/Section.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Section
+    {
+    }
+}
