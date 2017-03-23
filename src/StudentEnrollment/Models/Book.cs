9fccaf6f0188b24bb9e156904e9b11bb5799c505
ms8565 <ms8565@rit.edu>
2017-03-23 16:45:22 -0400

    Added book class

---
+++ src/StudentEnrollment/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Book
+    {
+        public Book(int id, int isbn, String title)
+        {
+            ID = id;
+            ISBN = isbn;
+            Title = title;
+        }
+
+        public int ID { get; }
+        public int ISBN { get; }
+        public String Title { get; }
+    }
+}
