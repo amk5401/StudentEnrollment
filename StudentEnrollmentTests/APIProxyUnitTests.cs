35c05c28fa5ba7b715a8fa6ab46337e59271adbc
marissa <mxb9328@g.rit.edu>
2017-03-23 21:45:23 -0400

    added test directory and stubbed out LocalProxy tests

---
+++ StudentEnrollmentTests/APIProxyUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentEnrollmentTests
+{
+    [TestClass]
+    public class APIProxyUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
