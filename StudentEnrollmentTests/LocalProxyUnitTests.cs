7ece595926d9817640c6a709194cd8938233f715
marissa <mxb9328@g.rit.edu>
2017-03-26 17:28:36 -0400

    tests

--- StudentEnrollmentTests/LocalProxyUnitTests.cs
+++ StudentEnrollmentTests/LocalProxyUnitTests.cs
@@ -1,10 +1,123 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentEnrollment;
 
 namespace StudentEnrollmentTests
 {
-    class LocalProxtUnitTests
+    [TestClass]
+    class LocalProxyUnitTests
     {
+        [TestMethod]
+        public void createBookUnitTest()
+        {
+
+        }
+
+        public void createCourseUnitTest()
+        {
+
+        }
+
+        public void createSectionUnitTest()
+        {
+
+        }
+
+        public void createStudentUnitTest()
+        {
+
+        }
+
+        public void createTermUnitTest()
+        {
+
+        }
+        
+        public void deleteSectionUnitTest()
+        {
+
+        }
+
+        public void enrollStudentUnitTest()
+        {
+
+        }
+        
+        public void getAdminUnitTest()
+        {
+
+        }
+
+        public void getBookUnitTest()
+        {
+
+        }
+
+        public void getCourseUnitTest()
+        {
+
+        }
+
+        public void getCourseListUnitTest()
+        {
+
+        }
+
+        public void toggleCourseUnitTest()
+        {
+
+        }
+
+        public void getSectionUnitTest()
+        {
+
+        }
+
+        public void getCourseSectionsUnitTest()
+        {
+
+        }
+
+        public void getStudentSectionsUnitTest()
+        {
+
+        }
+
+        public void getInstructorSectionsUnitTest()
+        {
+
+        }
+
+        public void getSectionBooksUnitTest()
+        {
+
+        }
+
+        public void getLocationUnitTest()
+        {
+
+        }
+
+        public void getTermUnitTest()
+        {
+
+        }
+        
+        public void getCurrentTermUnitTest()
+        {
+
+        }
+
+        public void waitlistStudentUnitTest()
+        {
+
+        }
+
+        public void withdrawStudentUnitTest()
+        {
+
+        }
+
+
     }
 }
