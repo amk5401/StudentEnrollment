9f08d7320ba57a5f3466dc057926fb8222d45d81
Madison Smith (RIT Student) <ms8565@ad.rit.edu>
2017-03-30 20:18:38 -0400

    adding test files

---
+++ StudentEnrollment.Tests/ModelFactoryUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentEnrollment.Models;
+
+namespace StudentEnrollment.Tests
+{
+    [TestClass]
+    public class ModelFactoryUnitTests
+    {
+        [TestMethod]
+        public void createModelUnitTest()
+        {
+            String modelType = "student";
+            String json = "{'ID': 0, 'USERNAME': 'bob32', 'EMAIL' : 'bob@email.com', 'FIRSTNAME': 'Bob', 'LASTNAME': 'Johnson', 'YEAR_LEVEL': 2,'GPA': 3.5}";
+        Model model = ModelFactory.createModelFromJson(modelType, json);
+
+            Student student = (Student)model;
+
+            Assert.IsNotNull(student);
+
+            Assert.AreEqual(student.FirstName, "Bob");
+            Assert.AreEqual(student.LastName, "Johnson");
+        }
+        
+    }
+}
---
+++ StudentEnrollment.Tests/ProxyUnitTests/LocalProxyUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentEnrollment;
+
+namespace StudentEnrollment.Tests
+{
+    [TestClass]
+    public class LocalProxyUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
