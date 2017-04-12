e6bbb202d60129229ae1ca5b32295ae49e80c52b
Austin Klein <amk5401@rit.edu>
2017-04-11 20:30:05 -0400

    Added course detail page
    page is not able to be edited yet

--- StudentEnrollment/Controllers/SectionController.cs
+++ StudentEnrollment/Controllers/SectionController.cs
@@ -120,30 +120,5 @@ namespace StudentEnrollment.Controllers
             this.proxy.waitlistStudent(student, section);
             return RedirectToAction("SectionList", "Section", new { courseID = section.CourseID });
         }
-        public ActionResult SectionDetails(int sectionID)
-        {
-            IProxy proxy = new APIProxy();
-
-
-            ViewData["Title"] = (proxy.getSection(sectionID).Course);
-            Console.Write(proxy.getSection(sectionID));
-
-            // TODO: uncomment and make sure this works with the API proxy after the endpoint is made
-            /* Section section = proxy.getSection(sectionID);
-            Instructor instructor = proxy.getInstructor(section.InstructorID);
-            //ViewData["Instructor"] = proxy.getInstructor(proxy.getSection(sectionID).InstructorID);
-            ViewData["Instructor"] = instructor;
-            //return View();
-            */
-
-            //only keeping this in to test how view shows instructor
-            Instructor i = new Instructor(4, "prof4", "prof@example", "Dan", "Krutz");
-            Section s = (proxy.getSection(sectionID));
-            Course c = proxy.getCourse(s.CourseID);
-            ViewData["InstructorName"] = i.LastName;
-            ViewData["CourseCode"] = c.CourseCode;
-            return View(proxy.getSection(sectionID));
-
-        }
     }
 }
