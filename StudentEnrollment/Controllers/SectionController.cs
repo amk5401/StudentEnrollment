d4b1e8ae897f3bde5621bdd052269996064fb6ee
marissa <mxb9328@g.rit.edu>
2017-04-11 21:58:32 -0400

    added instructor and course code to section view

--- StudentEnrollment/Controllers/SectionController.cs
+++ StudentEnrollment/Controllers/SectionController.cs
@@ -34,8 +34,22 @@ namespace StudentEnrollment.Controllers
         {
             IProxy proxy = new APIProxy();
 
+
             ViewData["Title"] = (proxy.getSection(sectionID).Course);
+            Console.Write(proxy.getSection(sectionID));
+
+            /* Section section = proxy.getSection(sectionID);
+            Instructor instructor = proxy.getInstructor(section.InstructorID);
+            //ViewData["Instructor"] = proxy.getInstructor(proxy.getSection(sectionID).InstructorID);
+            ViewData["Instructor"] = instructor;
             //return View();
+            */
+
+            Instructor i = new Instructor(4, "prof4", "prof@example", "Dan", "Krutz");
+            Section s = (proxy.getSection(sectionID));
+            Course c = proxy.getCourse(s.CourseID);
+            ViewData["InstructorName"] = i.LastName;
+            ViewData["CourseCode"] = c.CourseCode;
             return View(proxy.getSection(sectionID));
 
         }
