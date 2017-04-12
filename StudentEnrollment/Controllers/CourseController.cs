4b523effd42019f53f1a67a6de157e6d278c5f46
Austin Klein <amk5401@rit.edu>
2017-04-11 20:30:05 -0400

    Added course detail page
    page is not able to be edited yet

--- StudentEnrollment/Controllers/CourseController.cs
+++ StudentEnrollment/Controllers/CourseController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using StudentEnrollment.Proxy;
+using StudentEnrollment.Models;
 
 namespace StudentEnrollment.Controllers
 {
@@ -13,19 +14,17 @@ namespace StudentEnrollment.Controllers
         public ActionResult CourseList()
         {
             ViewData["Title"] = "Course List";
-
-            int[] none = new int[] { };
-            int[] se = new int[] {3, 4};
-            Models.Course c1 = new StudentEnrollment.Models.Course(1, "SWEN-344", "Web Engineering", 3, 2, true);
-            Models.Course c2 = new StudentEnrollment.Models.Course(2, "CS-420", "Data Mining", 3, 3, true);
-            Models.Course c3 = new StudentEnrollment.Models.Course(3, "SWEN-261", "Intro to SE", 3, 1, true);
-            Models.Course c4 = new StudentEnrollment.Models.Course(4, "CS-262", "Engineering of Software Subsystems", 3, 1, true);
-            List<StudentEnrollment.Models.Course> courses = new List<Models.Course> { c1, c2, c3, c4 };
-
-
             IProxy p = new APIProxy();
-            //ViewData["Courses"] = p.getCourseList();
             return View(p.getCourseList());
+        }
+
+        public ActionResult CourseDetail(int courseID)
+        {
+            ViewData["Title"] = "Course Detail";
+            IProxy p = new APIProxy();
+            Course course = p.getCourse(courseID);
+            //CourseAndSectionModel courseAndSectionModel = new CourseAndSectionModel(course, p.getCourseSections(course));
+            return View(course);
         }
     }
 }
