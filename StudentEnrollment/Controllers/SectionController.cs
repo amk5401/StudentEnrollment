340132246a03ef2848d2c192fcc3e8e11f5f9b5f
marissa <mxb9328@g.rit.edu>
2017-04-23 13:46:40 -0400

    adding course name and instructor name to section details view

--- StudentEnrollment/Controllers/SectionController.cs
+++ StudentEnrollment/Controllers/SectionController.cs
@@ -24,24 +24,24 @@ namespace StudentEnrollment.Controllers
             IProxy proxy = new APIProxy();
 
 
-            ViewData["Title"] = (proxy.getSection(sectionID).Course);
+            ViewData["Title"] = (proxy.getSection(sectionID).CourseID);
             Console.Write(proxy.getSection(sectionID));
 
-            // TODO: uncomment and make sure this works with the API proxy after the endpoint is made
-            /* Section section = proxy.getSection(sectionID);
+            
+            Section section = proxy.getSection(sectionID);
+            Course c = proxy.getCourse(section.CourseID);
             Instructor instructor = proxy.getInstructor(section.InstructorID);
-            //ViewData["Instructor"] = proxy.getInstructor(proxy.getSection(sectionID).InstructorID);
-            ViewData["Instructor"] = instructor;
-            //return View();
-            */
 
-            //only keeping this in to test how view shows instructor
-            Instructor i = new Instructor(4, "prof4", "prof@example", "Dan", "Krutz");
-            Section s = (proxy.getSection(sectionID));
-            Course c = proxy.getCourse(s.CourseID);
-            ViewData["InstructorName"] = i.LastName;
+
+            ViewData["Instructor"] = instructor;
+            ViewData["Course"] = c;
             ViewData["CourseCode"] = c.CourseCode;
+            ViewData["CourseName"] = c.Name;
             return View(proxy.getSection(sectionID));
+        
+        
+            
+            
 
         }
     }
