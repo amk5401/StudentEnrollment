cd5d3d8ab9b390ae3662d45bdbe694f4d40a648f
Austin Klein <amk5401@rit.edu>
2017-04-25 22:08:52 -0400

    edit course detail only for admins

--- StudentEnrollment/Controllers/CourseController.cs
+++ StudentEnrollment/Controllers/CourseController.cs
@@ -14,15 +14,20 @@ namespace StudentEnrollment.Controllers
         public ActionResult CourseList()
         {
             ViewData["Title"] = "Course List";
-            IProxy p = new APIProxy();
+            APIProxy p = new APIProxy();
             return View(p.getCourseList());
         }
 
         public ActionResult CourseDetail(int courseID)
         {
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("Index", "Login", new { redirectAction = "CourseDetail", redirectController = "Course", courseID = courseID });
+            }
             ViewData["Title"] = "Course Detail";
-            IProxy p = new APIProxy();
+            APIProxy p = new APIProxy();
             Course course = p.getCourse(courseID);
+            ViewData["Role"] = Session["role"];
             //CourseAndSectionModel courseAndSectionModel = new CourseAndSectionModel(course, p.getCourseSections(course));
             return View(course);
         }
@@ -32,8 +37,8 @@ namespace StudentEnrollment.Controllers
         {
             if (ModelState.IsValid)
             {
-                IProxy p = new APIProxy();
-                //p.createCourse(model);//p.editCourse(model);
+                APIProxy p = new APIProxy();
+                p.updateCourse(model);//p.createCourse(model);
                 return RedirectToAction("CourseDetail", new { courseId = model.ID });
             } else
             {
