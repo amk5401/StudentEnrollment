0bb797bde85662f954c07a5e0c4e2d470f802ada
Hunter Caskey <huntercaskey@gmail.com>
2017-05-01 10:46:14 -0400

    Started the implementation for an admin-only remove course button on the Course Detail page.

--- StudentEnrollment/Controllers/CourseController.cs
+++ StudentEnrollment/Controllers/CourseController.cs
@@ -10,12 +10,28 @@ namespace StudentEnrollment.Controllers
 {
     public class CourseController : Controller
     {
+        private APIProxy proxy = new APIProxy();
+
+        private bool loggedIn()
+        {
+            if (Session["user"] == null) return false; else return true;
+        }
+
+        private bool isAdmin()
+        {
+            if (Session["user"] != null && Session["role"] != null && String.Equals("admin", (string)Session["Role"], StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
         // GET: /<controller>/
         public ActionResult CourseList()
         {
             ViewData["Title"] = "Course List";
-            APIProxy p = new APIProxy();
-            return View(p.getCourseList());
+            return View(proxy.getCourseList());
         }
 
         public ActionResult CourseDetail(int courseID)
@@ -25,8 +41,7 @@ namespace StudentEnrollment.Controllers
                 return RedirectToAction("Index", "Login", new { redirectAction = "CourseDetail", redirectController = "Course", courseID = courseID });
             }
             ViewData["Title"] = "Course Detail";
-            APIProxy p = new APIProxy();
-            Course course = p.getCourse(courseID);
+            Course course = proxy.getCourse(courseID);
             ViewData["Role"] = Session["role"];
             //CourseAndSectionModel courseAndSectionModel = new CourseAndSectionModel(course, p.getCourseSections(course));
             return View(course);
@@ -37,13 +52,23 @@ namespace StudentEnrollment.Controllers
         {
             if (ModelState.IsValid)
             {
-                APIProxy p = new APIProxy();
-                p.updateCourse(model);//p.createCourse(model);
+                proxy.updateCourse(model);//p.createCourse(model);
                 return RedirectToAction("CourseDetail", new { courseId = model.ID });
             } else
             {
                 return RedirectToAction("CourseList");
             }
+        }
+
+        [HttpPost]
+        public ActionResult RemoveCourse(int courseID)
+        {
+            if (!loggedIn()) return RedirectToAction("Index", "Login", new { redirectAction = "CourseDetail", redirectController = "Course", courseID = courseID });
+            if (!isAdmin()) return RedirectToAction("AccessDenied", "Home");
+
+            //proxy.deleteCourse(courseID);
+            // Check?
+            return CourseList();
         }
     }
 }
